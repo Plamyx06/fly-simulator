04095ed36efbbaccb623fc402ca06e7ed59fd327
Aurélien A <albanese.aurelien06@gmail.com>
2024-06-17 14:37:23 +0200

    test

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_API_KEY_CESIUM: string
+  readonly VITE_APP_API_KEY_GOOGLE_MAPS: string
+  readonly BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_APP_API_KEY_CESIUM: string
-  readonly VITE_APP_API_KEY_GOOGLE_MAPS: string
-  readonly BASE_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
